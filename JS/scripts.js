add2d2360d578a77101fe92896543752f99eb7a1
Angels101 <angelscodex101@gmail.com>
2020-07-22 22:52:17 +0300

    the addition of a fuction to display this is a paragraph alert

--- JS/scripts.js
+++ JS/scripts.js
@@ -9,4 +9,7 @@ jQuery('h1').click(function(){
   jQuery('img').click(function(){
     alert("This is an image")
   });
-  
+  jQuery("h1").click(function() {
+    alert("This is a header.");
+    alert("I told you, THIS IS A HEADER!");
+  });
